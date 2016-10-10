e199444a89b3ff7ebff6d703e4f8867b6eeb7ae2
bluebug <iopzx@gmail.com>
2016-10-10 13:53:21 -0400

    Need to create new entity class that can be used as a base class for every game object.
    This is a phase where we start to plan out things and work on it. Try
    not to use raw classes we created from in classes because they are meant
    to be used only for a quick dirty way of presenting something on screen.

--- SpaceWars/Camera.cpp
+++ SpaceWars/Camera.cpp
@@ -6,15 +6,16 @@ using namespace DirectX;
 
 Camera::Camera(float aspectRatio)
 {
-	transform = new Transform();
+	transform = new Transform(); //TODO get rid of this new line by using unique smart pointer. For now this line is creating memory leak.
 	transform->SetPosition(0, 0, -8);
 	transform->SetRotation(0, 0, 1);
 	this->aspectRatio = aspectRatio;
-	UpdateViewMatrix();
+	//UpdateViewMatrix();
 }
 
 void Camera::SetAspectRatio(float aspectRatio) 
 {
+	isDirtyCameraProjection = true;
 	this->aspectRatio = aspectRatio;
 
 	// Create/Update the Projection matrix
@@ -25,108 +26,99 @@ void Camera::SetAspectRatio(float aspectRatio)
 		aspectRatio,			// Aspect ratio
 		0.1f,				  	// Near clip plane distance
 		100.0f);			  	// Far clip plane distance
-	XMStoreFloat4x4(&projectionMatrix, XMMatrixTranspose(P)); // Transpose for HLSL!
+	XMStoreFloat4x4(&matrixProjection, XMMatrixTranspose(P)); // Transpose for HLSL!
 }
 
-Camera::~Camera()
+DirectX::XMFLOAT4X4 Camera::getViewMatrix()
 {
-	if (transform != nullptr) {
-		delete transform;
-		transform = nullptr;
+	if (isDirtyCameraView) {
+		isDirtyCameraView = false;
+		//update the view matrix
 	}
+	return matrixView;
 }
 
+DirectX::XMFLOAT4X4 Camera::getProjectionMatrix()
+{
+	if (isDirtyCameraProjection) {
+		//update projection
+		isDirtyCameraProjection = false;
+	}
+	return matrixProjection;
+}
+
+/*
 void Camera::Update(double deltaTime, double elapsedTime)
 {
-	float speed = 15.0f;
-	if (GetAsyncKeyState('W') & 0x8000) {
-		this->Advance((float)deltaTime * speed);
-	}
-
-	if (GetAsyncKeyState('S') & 0x8000) {
-		this->Advance((float)-deltaTime * speed);
-	}
-
-	if (GetAsyncKeyState('A') & 0x8000) {
-		this->Strafe((float)deltaTime * speed);
-	}
-
-	if (GetAsyncKeyState('D') & 0x8000) {
-		this->Strafe((float)-deltaTime * speed);
-	}
-
-	if (GetAsyncKeyState('E') & 0x8000) {
-		this->Ascend((float)deltaTime * speed);
-	}
-
-	if (GetAsyncKeyState('C') & 0x8000) {
-		this->Ascend((float)-deltaTime * speed);
-	}
-
-	UpdateViewMatrix();
 }
+*/
 
+
+
+
+/*
 void Camera::UpdateViewMatrix()
 {
-	// Create the View matrix
-	// - In an actual game, recreate this matrix every time the camera 
-	//    moves (potentially every frame)
-	// - We're using the LOOK TO function, which takes the position of the
-	//    camera and the direction vector along which to look (as well as "up")
-	// - Another option is the LOOK AT function, to look towards a specific
-	//    point in 3D space
-	XMVECTOR pos = XMLoadFloat3(&transform->GetPosition());
+// Create the View matrix
+// - In an actual game, recreate this matrix every time the camera
+//    moves (potentially every frame)
+// - We're using the LOOK TO function, which takes the position of the
+//    camera and the direction vector along which to look (as well as "up")
+// - Another option is the LOOK AT function, to look towards a specific
+//    point in 3D space
+XMVECTOR pos = XMLoadFloat3(&transform->GetPosition());
 
-	XMFLOAT3 rotation = transform->GetRotation();
-	XMVECTOR dir = XMQuaternionRotationRollPitchYaw( rotation.x, rotation.y, 3.14f );
-	XMVECTOR up = XMVectorSet(0, 1, 0, 0);
-	XMMATRIX V = XMMatrixLookToLH(
-		pos,     // The position of the "camera"
-		dir,     // Direction the camera is looking
-		up);     // "Up" direction in 3D space (prevents roll)
-	XMStoreFloat4x4(&viewMatrix, XMMatrixTranspose(V)); // Transpose for HLSL!
+XMFLOAT3 rotation = transform->GetRotation();
+XMVECTOR dir = XMQuaternionRotationRollPitchYaw( rotation.x, rotation.y, 3.14f );
+XMVECTOR up = XMVectorSet(0, 1, 0, 0);
+XMMATRIX V = XMMatrixLookToLH(
+pos,     // The position of the "camera"
+dir,     // Direction the camera is looking
+up);     // "Up" direction in 3D space (prevents roll)
+XMStoreFloat4x4(&viewMatrix, XMMatrixTranspose(V)); // Transpose for HLSL!
 }
 
 Vector3 Camera::getForward() {
-	Vector3 rotation = transform->GetRotation();
-	//Rotation Quaternion - the camera behaves properly w/ YXZ instead of XYZ
-	//Alos not sure why it needs to be x/2, y/2 (and probably z/2)
-	Quaternion rot = Quaternion::CreateFromYawPitchRoll(rotation.y / 2, rotation.x, rotation.z / 2);
-	return Vector3::Transform(Vector3(0, 0, 1), rot);
+Vector3 rotation = transform->GetRotation();
+//Rotation Quaternion - the camera behaves properly w/ YXZ instead of XYZ
+//Alos not sure why it needs to be x/2, y/2 (and probably z/2)
+Quaternion rot = Quaternion::CreateFromYawPitchRoll(rotation.y / 2, rotation.x, rotation.z / 2);
+return Vector3::Transform(Vector3(0, 0, 1), rot);
 }
 
 //Move the camera forward(+)/back(-) the by the given amount
 void Camera::Advance(float units)
 {
-	Vector3 position = transform->GetPosition();
-	Vector3 forward = getForward();
-	position += forward * units;
-	transform->SetPosition(position);
+Vector3 position = transform->GetPosition();
+Vector3 forward = getForward();
+position += forward * units;
+transform->SetPosition(position);
 }
 
 //Move the camera right(+)/left(-) by the given amount
 void Camera::Strafe(float units)
 {
-	Vector3 position = transform->GetPosition();
-	Vector3 forward = getForward();
-	XMVECTOR right = XMVector3Cross(forward, XMVectorSet(0, 1, 0, 0));
-	position += right * units;
-	transform->SetPosition(position);
+Vector3 position = transform->GetPosition();
+Vector3 forward = getForward();
+XMVECTOR right = XMVector3Cross(forward, XMVectorSet(0, 1, 0, 0));
+position += right * units;
+transform->SetPosition(position);
 }
 
 //Move the camera up(+) or down(-) the world Y axis by the given amount
 void Camera::Ascend(float units)
 {
-	XMVECTOR position = XMLoadFloat3(&transform->GetPosition());
-	position += XMVectorSet(0, 1, 0, 0) * units;
-	transform->SetPosition(position);
+XMVECTOR position = XMLoadFloat3(&transform->GetPosition());
+position += XMVectorSet(0, 1, 0, 0) * units;
+transform->SetPosition(position);
 }
 
 void Camera::Rotate(float x, float y)
 {
-	XMFLOAT3 rotation = transform->GetRotation();
-	//I'm not sure why a full rotation is 4*PI instead of 2*PI...
-	//This is probably related to needing to divide rotation by 2 in get forward
-	float FOUR_PI = 12.56f; 
-	transform->SetRotation(fmod(rotation.x + x, FOUR_PI), fmod(rotation.y + y, FOUR_PI), 0);
+XMFLOAT3 rotation = transform->GetRotation();
+//I'm not sure why a full rotation is 4*PI instead of 2*PI...
+//This is probably related to needing to divide rotation by 2 in get forward
+float FOUR_PI = 12.56f;
+transform->SetRotation(fmod(rotation.x + x, FOUR_PI), fmod(rotation.y + y, FOUR_PI), 0);
 }
+*/
--- SpaceWars/Camera.h
+++ SpaceWars/Camera.h
@@ -8,34 +8,42 @@ class Camera
 	float aspectRatio;
 	Transform* transform;
 
-	DirectX::XMFLOAT4X4 viewMatrix;
-	DirectX::XMFLOAT4X4 projectionMatrix;
+	bool	isDirtyCameraView,
+			isDirtyCameraProjection;
 
-	Vector3 getForward();
+	DirectX::XMFLOAT4X4 matrixView;
+	DirectX::XMFLOAT4X4 matrixProjection;
+	/*
+		Vector3 getForward(); 
+		is this a helper function? or a function that is more important than as a helper function? hprGetForward() hprNameOfFunction would be great.
+		prefixs help to understand what the function purpose is 
+	*/
 public:
 	Camera(float aspectRatio);
-	~Camera();
+	
 
-	void Update(double deltaTime, double elapsedTime);
+	/*
+		void Update(double deltaTime, double elapsedTime);
+			If camera class was to be part of "game object", which is what you seem to be doing because you extended from transform, 
+			it has to as well extend a class that has update method rather than having its own
+
+		void Advance(float units);
+		void Strafe(float units);
+		void Ascend(float units);
+		void Rotate(float x, float y);
+			These functions are basically the same in that they transform the transform class.
+
+	*/
 
 	void SetAspectRatio(float aspectRatio);
 
-	void UpdateViewMatrix();
+	/*
+		void UpdateViewMatrix();
+		try what Transform class did. 
+	*/
 
-	void Advance(float units);
+	DirectX::XMFLOAT4X4 getViewMatrix();
 
-	void Strafe(float units);
-
-	void Ascend(float units);
-
-	void Rotate(float x, float y);
-
-	DirectX::XMFLOAT4X4 getViewMatrix() {
-		return viewMatrix;
-	}
-
-	DirectX::XMFLOAT4X4 getProjectionMatrix() {
-		return projectionMatrix;
-	}
+	DirectX::XMFLOAT4X4 getProjectionMatrix();
 };
 
