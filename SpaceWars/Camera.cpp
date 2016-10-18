41fc4a8823cc401691e9c60cf9350934eaf1f0c0
Greg Rozmarynowycz <crashtest1@live.com>
2016-10-17 20:56:32 -0400

    fix camera

--- SpaceWars/Camera.cpp
+++ SpaceWars/Camera.cpp
@@ -76,14 +76,11 @@ void Camera::updateViewMatrix()
 	// - Another option is the LOOK AT function, to look towards a specific
 	//    point in 3D space
 	XMVECTOR pos = XMLoadFloat3(&transform->GetPosition());
-
-	XMFLOAT3 rotation = transform->GetRotation();
-	XMVECTOR dir = XMQuaternionRotationRollPitchYaw( rotation.x, rotation.y, 3.14f );
 	XMVECTOR up = XMVectorSet(0, 1, 0, 0);
 	XMMATRIX V = XMMatrixLookToLH(
-		pos,     // The position of the "camera"
-		dir,     // Direction the camera is looking
-		up);     // "Up" direction in 3D space (prevents roll)
+		pos,			// The position of the "camera"
+		getForward(),   // Direction the camera is looking
+		up);			// "Up" direction in 3D space (prevents roll)
 	XMStoreFloat4x4(&viewMatrix, XMMatrixTranspose(V)); // Transpose for HLSL!
 }
 
@@ -91,7 +88,7 @@ Vector3 Camera::getForward() {
 	Vector3 rotation = transform->GetRotation();
 	//Rotation Quaternion - the camera behaves properly w/ YXZ instead of XYZ
 	//Alos not sure why it needs to be x/2, y/2 (and probably z/2)
-	Quaternion rot = Quaternion::CreateFromYawPitchRoll(rotation.y / 2, rotation.x, rotation.z / 2);
+	Quaternion rot = Quaternion::CreateFromYawPitchRoll(rotation.y, rotation.x, rotation.z);
 	return Vector3::Transform(Vector3(0, 0, 1), rot);
 }
 
@@ -127,6 +124,6 @@ void Camera::rotate(float x, float y)
 	XMFLOAT3 rotation = transform->GetRotation();
 	//I'm not sure why a full rotation is 4*PI instead of 2*PI...
 	//This is probably related to needing to divide rotation by 2 in get forward
-	float FOUR_PI = 12.56f; 
-	transform->SetRotation(fmod(rotation.x + x, FOUR_PI), fmod(rotation.y + y, FOUR_PI), 0);
+	float TWO_PI = 6.28f; 
+	transform->SetRotation(fmod(rotation.x + x, TWO_PI), fmod(rotation.y + y, TWO_PI), 0);
 }
